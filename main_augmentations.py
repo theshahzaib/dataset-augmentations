ef62835d4539e905ce9bdc303ce81b02576951b3
Shahzaib <51841651+m3zai6i@users.noreply.github.com>
2022-12-06 14:37:46 +0500

    init

--- main_augmentations.py
+++ main_augmentations.py
@@ -1,105 +1,18 @@
-
-# from ast import arg
-# from distutils import text_file
-# from os.path import exists
-# from tkinter import Label
 import Augmentation_backend as am
-# import Helpers_ as hp
+import Helpers_ as hp
 import cv2
 from helpers import *
 import glob, os
 import argparse
-import math
+import math, random
 # import imgaug as ia
 import imgaug.augmenters as iaa
 import numpy as np
-# import PIL.Image
+import PIL.Image
 # import torch
 # from torchvision import transforms
 import shutil
 from tqdm import tqdm
-
-# def augment(image, output_path, txt_path, image_name, args):
-
-#     if args.aug_type == 'brightness':
-#         image = am.brighten(image, brightness_coeff=0.3)
-
-#     if args.aug_type == 'darken':
-#         am.darken(image, darkness_coeff=0.1)
-
-#     if args.aug_type == 'add_shadow':
-#         image = am.add_shadow(image, no_of_shadows=2, shadow_dimension=8)
-
-#     if args.aug_type == 'add_rain':
-#         image = am.add_rain(image, rain_type='heavy', slant=20)
-
-#     if args.aug_type == 'add_fog':
-#         image = am.add_fog(image, fog_coeff=0.3)
-
-#     if args.aug_type == 'add_gravel':
-#         image = am.add_gravel(image, rectangular_roi=(700, 550, 1280, 720), no_of_patches=20)
-
-#     if args.aug_type == 'add_sun_flare':
-#         image = am.add_sun_flare(image, flare_center=(100, 100), angle=-math.pi / 4)
-
-#     if args.aug_type == 'add_apeed':
-#         image = am.add_speed(image, speed_coeff=0.9)
-
-#     if args.aug_type == 'add_autumn':
-#         image = am.add_autumn(image)
-
-#     if args.aug_type == 'hsv':
-#         image1 = image.copy()
-#         aug = iaa.imgcorruptlike.Saturate(severity=3)
-#         image = aug(image=image1)
-
-#     if args.aug_type=='green':
-#         img = image
-#         row, col, plane = img.shape
-#         temp = np.zeros((row, col, plane), np.uint8)
-#         temp[:, :, 1] = img[:, :, 1]
-#         image = temp.copy()
-
-#     if args.aug_type=='blue':
-#         img = image
-#         row, col, plane = img.shape
-#         temp = np.zeros((row, col, plane), np.uint8)
-#         temp[:, :, 1] = img[:, :, 0]
-#         image = temp.copy()
-
-#     if args.aug_type=='red':
-#         img = image
-#         row, col, plane = img.shape
-#         temp = np.zeros((row, col, plane), np.uint8)
-#         temp[:, :, 1] = img[:, :, 2]
-#         image = temp.copy()
-
-#     if args.aug_type=='add_blur':
-#         aug = iaa.imgcorruptlike.MotionBlur(severity=5)
-#         image = aug(image=image)
-
-#     if args.aug_type=='hue':
-#         aug = iaa.AddToHue((-50, 50))
-#         image = aug(image=image)
-
-#     if args.aug_type=='gaussian_noise':
-#         aug = iaa.imgcorruptlike.GaussianNoise(severity=5)
-#         image = aug(image=image)
-
-#     if args.aug_type=='linearcontrast':
-#         aug = iaa.LinearContrast((0.75, 1.5))
-#         image = aug(image=image)
-
-#     if args.aug_type=='change_temperature':
-#         aug = iaa.ChangeColorTemperature((1100, 10000))
-#         image = aug(image=image)
-
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#     cv2.imwrite(output_path + args.aug_type + '_' + image_name, image)
-#     name = image_name.split('.')[0]
-#     shutil.copy(txt_path, output_path + name + '.txt')
-#     os.rename(output_path + name + '.txt', output_path + args.aug_type + '_' + name + '.txt')
-
 
 def augment_all_one(image, output_path, txt_path, image_name, aug_):
 
@@ -107,7 +20,7 @@ def augment_all_one(image, output_path, txt_path, image_name, aug_):
         image = am.brighten(image, brightness_coeff=0.3)
 
     if aug_ == 'darken':
-        am.darken(image, darkness_coeff=0.1)
+        image = am.darken(image, darkness_coeff=0.1)
 
     if aug_ == 'add_shadow':
         image = am.add_shadow(image, no_of_shadows=2, shadow_dimension=8)
@@ -121,11 +34,11 @@ def augment_all_one(image, output_path, txt_path, image_name, aug_):
     if aug_ == 'add_gravel':
         image = am.add_gravel(image, rectangular_roi=(700, 550, 1280, 720), no_of_patches=20)
 
-    if aug_ == 'add_sun_flare':
-        image = am.add_sun_flare(image, flare_center=(100, 100), angle=-math.pi / 4)
+    # if aug_ == 'add_sun_flare':
+    #     image = am.add_sun_flare(image, flare_center=(100, 100), angle= -(math.pi)/4)
 
-    if aug_ == 'add_apeed':
-        image = am.add_speed(image, speed_coeff=0.9)
+    # if aug_ == 'add_speed':
+    #     image = am.add_speed(image, speed_coeff=0.9)
 
     if aug_ == 'add_autumn':
         image = am.add_autumn(image)
@@ -211,20 +124,21 @@ if __name__ == "__main__":
     if os.path.exists('augmented_images'):
         shutil.rmtree('augmented_images')
     
-    print('Augmenting images...')
-    for aug_ in aug_type_list:
+    # print('Augmenting images...')
+    for aug_ in tqdm(aug_type_list, desc='Augmenting images', position=0):
 
         output_path = 'augmented_images/' + aug_ + '/'
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
-        print(f'Augmenting images with {aug_}...')
-        for img_path in tqdm(images_list):
+        # print(f'Augmenting images with {aug_}...')
+        for img_path in tqdm(images_list, desc=f'Augmenting images with {aug_}', position=1, leave=True):
             # print(img_path)
             image_name = os.path.basename(img_path)
             txt_name = image_name.split('.')[0]
             txt_path = args.images + txt_name + '.txt'
             image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+
             augment_all_one(image, output_path, txt_path, image_name, aug_)
             # print('Augmented image saved to {}'.format(output_path))
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-change_temperature
+if aug_ == 'add_sun_flare':
+        image = am.add_sun_flare(image, flare_center=(100, 100), angle= -(math.pi)/4)
+
+    if aug_ == 'add_speed':
+        image = am.add_speed(image, speed_coeff=0.9)
